ce3b752433f973a4ffc51ebec0554b8787b9fc48
RomeisaAliu <romeisa.aliu@albanialab.al>
2023-07-18 16:55:22 +0200

    deletes

--- src/main/java/com/example/email/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.email.repository;
-
-import com.example.email.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role,Long> {
-}
