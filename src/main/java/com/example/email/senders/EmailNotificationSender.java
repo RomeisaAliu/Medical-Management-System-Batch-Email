07b468dfb0baadb6c968a3f3deb6cc118666a31a
sildiRicku <sildi.ricku@albanialab.al>
2023-08-15 17:05:31 +0200

    final

--- src/main/java/com/example/email/senders/EmailNotificationSender.java
+++ src/main/java/com/example/email/senders/EmailNotificationSender.java
@@ -1,30 +1,70 @@
 package com.example.email.senders;
 
-import com.example.email.service.SendService;
+import com.example.email.helper.EmailProperties;
 
 
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 import lombok.Getter;
 import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Properties;
 
 @Getter
 @Setter
+@Component
 public class EmailNotificationSender implements NotificationSenderStrategy {
 
     private final Logger logger = LoggerFactory.getLogger(EmailNotificationSender.class);
-    private final SendService sendService;
+    private final EmailProperties emailProperties;
 
-    public EmailNotificationSender(SendService sendService) {
+    public EmailNotificationSender(EmailProperties emailProperties) {
 
-        this.sendService = sendService;
+        this.emailProperties = emailProperties;
     }
 
+    public void sendEmail(String email, String message) {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", emailProperties.getHost());
+        properties.put("mail.smtp.port", emailProperties.getPort());
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(emailProperties.getUsername(), emailProperties.getPassword());
+            }
+        });
+
+        try {
+            MimeMessage mimeMessage = new MimeMessage(session);
+
+            mimeMessage.setFrom(new InternetAddress(emailProperties.getUsername()));
+
+            mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(email));
+
+            mimeMessage.setSubject("Appointments for " + LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+
+            mimeMessage.setText(message);
+            Transport.send(mimeMessage);
+
+            logger.info("Email sent successfully to: {}", email);
+        } catch (MessagingException e) {
+            logger.error("Error sending email to: {}", email, e);
+        }
+    }
 
     @Override
     public void sendNotification(String doctorEmail, String message) {
         logger.error("Message sent via EMAIL to {}", doctorEmail);
-        sendService.sendEmail(doctorEmail, message);
+        sendEmail(doctorEmail, message);
     }
 
 }
--- src/main/java/com/example/email/service/SendService.java
+++ src/main/java/com/example/email/service/SendService.java
@@ -1,6 +1,5 @@
 package com.example.email.service;
 
-import com.example.email.helper.EmailProperties;
 import com.example.email.senders.EmailNotificationSender;
 import com.example.email.senders.NotificationSenderStrategy;
 import com.example.email.senders.SlackNotificationSender;
@@ -8,9 +7,6 @@ import com.example.email.senders.WhatsAppNotificationSender;
 import com.example.medicalmanagement.dto.UserDto;
 import com.example.medicalmanagement.model.NotificationType;
 
-import jakarta.mail.*;
-import jakarta.mail.internet.InternetAddress;
-import jakarta.mail.internet.MimeMessage;
 import lombok.Getter;
 import lombok.Setter;
 import org.slf4j.Logger;
@@ -18,8 +14,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 @Service
@@ -29,43 +23,13 @@ import java.util.*;
 public class SendService {
     private final Logger logger = LoggerFactory.getLogger(SendService.class);
     private NotificationSenderStrategy strategy;
-    private final EmailProperties emailProperties;
-    public SendService(EmailProperties emailProperties) {
+    private final EmailNotificationSender emailNotificationSender;
 
-        this.emailProperties = emailProperties;
+    public SendService(EmailNotificationSender emailNotificationSender) {
+
+        this.emailNotificationSender = emailNotificationSender;
     }
 
-    public void sendEmail(String email, String message) {
-        Properties properties = new Properties();
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
-        properties.put("mail.smtp.host", emailProperties.getHost());
-        properties.put("mail.smtp.port", emailProperties.getPort());
-
-        Session session = Session.getInstance(properties, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(emailProperties.getUsername(), emailProperties.getPassword());
-            }
-        });
-
-        try {
-            MimeMessage mimeMessage = new MimeMessage(session);
-
-            mimeMessage.setFrom(new InternetAddress(emailProperties.getUsername()));
-
-            mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(email));
-
-            mimeMessage.setSubject("Appointments for " + LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
-
-            mimeMessage.setText(message);
-            Transport.send(mimeMessage);
-
-            logger.info("Email sent successfully to: {}", email);
-        } catch (MessagingException e) {
-            logger.error("Error sending email to: {}", email, e);
-        }
-    }
 
     public void sendNotification(String doctorEmail, String message, UserDto userDto) {
 
@@ -73,7 +37,7 @@ public class SendService {
         for (NotificationType preference : types) {
             switch (preference) {
                 case EMAIL:
-                    strategy = new EmailNotificationSender(new SendService(emailProperties));
+                    strategy = new EmailNotificationSender(emailNotificationSender.getEmailProperties());
                     strategy.sendNotification(doctorEmail, message);
                     break;
                 case WHATSAPP:
